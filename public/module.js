419d36eec5c7cbfa280e4f9b0895bec100e85a19
xshunder@laptop <xshunder@outlook.com>
2017-12-23 16:39:23 +0800

    done myFac

--- public/module.js
+++ public/module.js
@@ -1,4 +1,4 @@
 'use strict';
 (function(){
-	angular.module('myApp',[]);
+	angular.module('myApp',['ngMaterial']);
 })();
--- public/myFac.js
+++ public/myFac.js
@@ -2,10 +2,47 @@
 (function(){
 	angular.module( 'myApp')
 		.factory('myFac', fac1);
-	function fac1($http){
+	function fac1($http,$log){
 		var self = {};
-		self.hello = function(){
-			$http.get( '/hello');
+		self.post =function(url,obj,cb){
+			$http.post(url,obj)
+				.then(function(res){
+					cb(null,res.data);
+				}, function(res){
+					cb(res);
+				});
+		}
+		self.get = function(url, cb){
+			$http.get(url)
+				.then( function(res){
+					cb(null,res.data);
+				}, function(res){
+					cb(res);
+				})
+		}
+		self.hello = function(cb){
+			self.get('hello',cb);
+		}
+		self.addUser = function(token,name,cb){
+			self.post('/add-user',{
+				face_token:token,
+				nickname,name
+			} ,cb);
+		}
+		self.login = function(token,cb){
+			self.post('/login',{
+				face_token:token
+			},cb);
+		}
+		self.postmsg = function( token,name,msg,cb){
+			self.post('msg',{
+				face_token:token,
+				msg:msg,
+				face_token:token
+			},cb);
+		}
+		self.getmsg = function( cb){
+			self.get('msg',cb);
 		}
 		return self;
 	}
